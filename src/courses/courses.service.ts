53b611bd39ef7173ee4aa921a6eae1cb748937d0
takagf <falcao130@gmail.com>
2024-07-08 23:50:37 -0300

    initial commit

---
+++ src/courses/courses.service.ts
@@ -0,0 +1,48 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Course } from "./courses.entity";
+
+@Injectable()
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: 'NestJS',
+            description: 'Curso sobre o Framework NestJS',
+            tags: ['node', 'nest', 'javascript', 'typescript']
+        }
+    ]
+
+    findAll() {
+        return this.courses
+    }
+
+    findOne(id: number) {
+        const course = this.courses.find(course => course.id === id )
+        if (!course) {
+            throw new NotFoundException(`Course ID ${id} not found`)
+        }
+        return course
+    }
+
+    create(createCourseDTO: any) {
+        this.courses.push(createCourseDTO)
+    }
+
+    update(id: number, updateCourseDTO: any) {
+        const existingCourse = this.findOne(id)
+        if (existingCourse as any) {
+            const index = this.courses.findIndex(course => course.id === id)
+            this.courses[index] = {
+                id,
+                ... updateCourseDTO,
+            }
+        }
+    }
+
+    remove(id: number) {
+        const index = this.courses.findIndex(course => course.id === id)
+        if (index >= 0) {
+            this.courses.splice(index, 1);
+        }
+    }
+}
